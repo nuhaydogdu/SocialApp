3af28c368349ff2935a661c714990ba851eb02b5
nuhaydogdu <nuhaydd25@gmail.com>
2023-07-12 22:38:28 +0300

    Registering User Information

---
+++ ServerApp/DTO/UserDTO.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServerApp.DTO
+{
+    public class UserDTO
+    {
+        public string Name {get; set;}
+        public string UserName {get; set;}
+        public string Email {get; set;}
+        public string Gender {get; set;}
+        public string Password {get; set;}
+
+    }
+}
